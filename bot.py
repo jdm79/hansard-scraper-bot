dd0572c5791fe0ac5427a7fc94b83f313450540b
jdm79 <james.malvern@gmail.com>
2020-03-20 00:40:02 +0000

    first commit

---
+++ bot.py
@@ -0,0 +1,53 @@
+import requests
+from requests import get
+from bs4 import BeautifulSoup
+import pandas as pd 
+import numpy as np 
+import tweepy
+import random
+import time
+import config
+
+def scraper():
+
+  auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+  auth.set_access_token(config.access_token, config.access_token_secret)
+ 
+  api = tweepy.API(auth)
+
+  headers = {"Accept-language": "en-US, en;q=0.5"}
+  url = "https://hansard.parliament.uk/lords/"
+  results = requests.get(url, headers=headers)
+
+  soup = BeautifulSoup(results.text, "html.parser")
+  issues_div = soup.find_all('li', class_='no-children')
+
+  issues = []
+  nada = "Nothing to see here. The lords are not debating."
+
+  for container in issues_div:
+    issue = container.a.text
+    issues.append(issue.strip())
+
+  string = ', '.join(sorted(issues))
+  intro = "Today, the Lords discussed: "
+  cont = "Cont'd... "
+
+  if len(string) > 0:
+    if len(string) < 260:
+      api.update_status(intro + string)
+      print(string)
+    else:
+      first, second = string[:len(string)//2], string[len(string)//2:]
+      api.update_status(intro + first)
+      print(intro + first)
+      time.sleep(5)
+      api.update_status(cont + second)
+      print(cont + second)
+  else:
+    api.update_status(nada)
+    print(nada)
+
+scraper()
+
+
