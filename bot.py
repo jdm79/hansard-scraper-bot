e998d29807bdc228f647eca25784b3d1a191c623
jdm79 <james.malvern@gmail.com>
2020-04-25 16:13:01 +0100

    copy across code from private repo

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import requests
 from requests import get
+from datetime import date
 from bs4 import BeautifulSoup
 import pandas as pd 
 import numpy as np 
@@ -8,13 +9,15 @@ import random
 import time
 import config
 
-def scraper():
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
+api = tweepy.API(auth)
 
-  auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
-  auth.set_access_token(config.access_token, config.access_token_secret)
- 
-  api = tweepy.API(auth)
-
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+today = date.today().isoweekday() -1
+day = days[today]
+  
+def scrape():
   headers = {"Accept-language": "en-US, en;q=0.5"}
   url = "https://hansard.parliament.uk/lords/"
   results = requests.get(url, headers=headers)
@@ -23,18 +26,21 @@ def scraper():
   issues_div = soup.find_all('li', class_='no-children')
 
   issues = []
-  nada = "Nothing to see here. The lords are not debating."
+  nada = "Nothing to report so far today."
 
   for container in issues_div:
     issue = container.a.text
     issues.append(issue.strip())
 
   string = ', '.join(sorted(issues))
-  intro = "Today, the Lords discussed: "
+  intro = day + ", the Lords discussed: "
   cont = "Cont'd... "
 
+# this needs to be improved:
+# clean up the split strings - don't split any words
+# deal with larger strings - ones that need three tweets or more 
   if len(string) > 0:
-    if len(string) < 260:
+    if len(string) < 250:
       api.update_status(intro + string)
       print(string)
     else:
@@ -48,6 +54,9 @@ def scraper():
     api.update_status(nada)
     print(nada)
 
-scraper()
-
-
+if today < 5:
+  print("it's " + day + " which is a weekday!")
+  scrape()
+else:
+  # print("The Lords do not debate on " + day + "s.")
+  api.update_status("The Lords do not debate on " + day + "s.")
