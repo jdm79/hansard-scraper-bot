bd4f24732264cc1acfb014d91c7a18187337ba2e
jdm79 <james.malvern@gmail.com>
2020-05-14 20:24:31 +0100

    splits the string into 4 equal parts now if it's too large, still need to fix splitting words

--- bot.py
+++ bot.py
@@ -10,8 +10,8 @@ import random
 import time
 import config
 
-auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
-auth.set_access_token(config.access_token, config.access_token_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
@@ -24,12 +24,14 @@ time_stamp = datetime.datetime.now()
 date_stamp = time_stamp.strftime("%Y-%m-%d")
 
 def scrape():
+
   results = requests.get(url + date_stamp, headers=headers)
 
   soup = BeautifulSoup(results.text, "html.parser")
   issues_div = soup.find_all('li', class_='no-children')
-  issues = []
+  issues_list = []
   nada = "Nothing to report so far today."
+
   # html_message = soup.find('p')
   # message_string = html_message.text.strip()
   # messages = message_string.split(".")
@@ -38,37 +40,49 @@ def scrape():
   #   # print(messages[0])
   #   api.update_status(messages[0])
   # else:
+
   for container in issues_div:
     issue = container.a.text
-    issues.append(issue.strip())
+    issues_list.append(issue.strip())
+
+  # make the items in the list unique
+  issues_set = set(issues_list)
+  issues_set.remove("House of Lords")
+
+  # convert back to list
+  issues = list(issues_set)
 
   string = ', '.join(sorted(issues))
+
   intro = day + ", the Lords discussed: "
   cont = "Cont'd... "
 
-  # print(messages[0])
-
-# this needs to be improved:
-# clean up the split strings - don't split any words
-# deal with larger strings - ones that need three tweets or more 
   if len(string) > 0:
     if len(string) < 250:
       api.update_status(intro + string)
-      print(intro + string)
     else:
       first, second = string[:len(string)//2], string[len(string)//2:]
-      api.update_status(intro + first)
-      print(intro + first)
-      time.sleep(5)
-      api.update_status(cont + second)
-      print(cont + second)
+      if len(first) < 280:
+        api.update_status(intro + first)
+        api.update_status(cont + second)
+      else:
+        first_frag, second_frag = first[:len(first)//2], first[len(first)//2:]
+        third_frag, fourth_frag = second[:len(second)//2], second[len(second)//2:]
+
+        time.sleep(3)
+
+        print(second_frag)
+        api.update_status(intro + first_frag)
+        api.update_status(cont + second_frag)
+
+        time.sleep(3)
+
+        api.update_status(cont + third_frag)
+        api.update_status(cont + fourth_frag)
   else:
     api.update_status(nada)
-    print(nada)
 
-# Only scrapes Mon-Thurs, as Lords don't sit on Fri, Sat and Sun
-if today < 5:
+if today < 4:
   scrape()
 else:
-  print("The Lords do not debate on " + day + "s.")
   api.update_status("The Lords do not debate on " + day + "s.")
