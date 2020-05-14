8a139a6918aafed8af87442864ad59d7e46e0577
James Malvern <james.malvern@gmail.com>
2020-05-14 20:27:16 +0100

    splits the string into four equal parts if too big for two tweets. still need to fix splitting words

--- bot.py
+++ bot.py
@@ -14,49 +14,74 @@ auth.set_access_token(config.access_token, config.access_token_secret)
 api = tweepy.API(auth)
 
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-today = date.today().isoweekday() -1
+today = date.today().isoweekday() - 1
 day = days[today]
   
+headers = {"Accept-language": "en-US, en;q=0.5"}
+url = "https://hansard.parliament.uk/lords/"
+time_stamp = datetime.datetime.now()
+date_stamp = time_stamp.strftime("%Y-%m-%d")
+
 def scrape():
-  headers = {"Accept-language": "en-US, en;q=0.5"}
-  url = "https://hansard.parliament.uk/lords/"
-  results = requests.get(url, headers=headers)
+
+  results = requests.get(url + date_stamp, headers=headers)
 
   soup = BeautifulSoup(results.text, "html.parser")
   issues_div = soup.find_all('li', class_='no-children')
-
-  issues = []
+  issues_list = []
   nada = "Nothing to report so far today."
+
+  # html_message = soup.find('p')
+  # message_string = html_message.text.strip()
+  # messages = message_string.split(".")
+  
+  # if (messages[0].find(".") == -1):
+  #   # print(messages[0])
+  #   api.update_status(messages[0])
+  # else:
 
   for container in issues_div:
     issue = container.a.text
-    issues.append(issue.strip())
+    issues_list.append(issue.strip())
+
+  # make the items in the list unique
+  issues_set = set(issues_list)
+  issues_set.remove("House of Lords")
+
+  # convert back to list
+  issues = list(issues_set)
 
   string = ', '.join(sorted(issues))
+
   intro = day + ", the Lords discussed: "
   cont = "Cont'd... "
 
-# this needs to be improved:
-# clean up the split strings - don't split any words
-# deal with larger strings - ones that need three tweets or more 
   if len(string) > 0:
     if len(string) < 250:
       api.update_status(intro + string)
-      print(string)
     else:
       first, second = string[:len(string)//2], string[len(string)//2:]
-      api.update_status(intro + first)
-      print(intro + first)
-      time.sleep(5)
-      api.update_status(cont + second)
-      print(cont + second)
+      if len(first) < 280:
+        api.update_status(intro + first)
+        api.update_status(cont + second)
+      else:
+        first_frag, second_frag = first[:len(first)//2], first[len(first)//2:]
+        third_frag, fourth_frag = second[:len(second)//2], second[len(second)//2:]
+
+        time.sleep(3)
+
+        print(second_frag)
+        api.update_status(intro + first_frag)
+        api.update_status(cont + second_frag)
+
+        time.sleep(3)
+
+        api.update_status(cont + third_frag)
+        api.update_status(cont + fourth_frag)
   else:
     api.update_status(nada)
-    print(nada)
 
-if today < 5:
-  print("it's " + day + " which is a weekday!")
+if today < 4:
   scrape()
 else:
-  # print("The Lords do not debate on " + day + "s.")
   api.update_status("The Lords do not debate on " + day + "s.")
